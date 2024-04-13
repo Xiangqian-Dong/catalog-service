1e88f765f2fd36453647de552d2b61fe3b592996
Dong Xiangqian <rasmol2@gmail.com>
2024-04-13 21:07:58 +0800

    ch5 done

--- src/test/java/com/ploarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/ploarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -4,18 +4,20 @@ import com.ploarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     void whenPostRequestThenBookCreated() {
-        var expectedBook = Book.of("1234567890", "Title", "Author", 9.90);
+        var expectedBook = Book.of("1234567890", "Title", "Author", 9.90, null);
         webTestClient.post().uri("/books")
                 .bodyValue(expectedBook)
                 .exchange()
